4d9132c5158556d7e7484819a86533f3c34a435f
naitoyuma7110 <naitoyuma1118@gmail.com>
2023-04-19 12:05:16 +0900

    first commit

---
+++ src/main/java/com/naito/techpit/chat/chatbachend/ChatBachendApplication.java
@@ -0,0 +1,13 @@
+package com.naito.techpit.chat.chatbachend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBachendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBachendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naito/techpit/chat/chatbachend/ChatBachendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naito.techpit.chat.chatbachend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBachendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
