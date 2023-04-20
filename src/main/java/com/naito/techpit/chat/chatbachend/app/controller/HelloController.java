92b5de21cd47ee101c56ea7de514bd4dd07e494b
Naito Yuma <128150297+naitoyuma7110@users.noreply.github.com>
2023-04-21 03:37:52 +0900

    Feature/#2 create demo test (#3)
    * [add]テスト用にコントローラーを修正、使用するHelloモデルを作成
    
    * [add]hello APIのテストを作成

--- src/main/java/com/naito/techpit/chat/chatbachend/app/controller/HelloController.java
+++ src/main/java/com/naito/techpit/chat/chatbachend/app/controller/HelloController.java
@@ -1,16 +1,21 @@
 package com.naito.techpit.chat.chatbachend.app.controller;
 
+import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 // import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.naito.techpit.chat.chatbachend.domain.hello.model.Hello;
 
 @RestController
 public class HelloController {
 
-  // @RequestMapping(path = "/hello", method = RequestMethod.GET)
   @GetMapping("/hello")
-  public String hello() {
-    return "Hello, world!";
+  public Hello hello(@RequestParam("name") Optional<String> name) {
+    //  Optional:null許容
+    //  orElse:optionalインスタンスがnullでない場合はその値を返す
+    String resName = name.orElse("world!");
+    return new Hello("Hello, " + resName);
   }
 }
