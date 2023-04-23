bb1a465b693080cd37969c3deed6de43f3869842
Naito Yuma <128150297+naitoyuma7110@users.noreply.github.com>
2023-04-23 14:19:29 +0900

    Revert "Feature/#6 create dynamic testing (#7)"
    This reverts commit 20c997ae768b297d3ce1cf33c1ece4e30ee7aca4.

--- src/test/java/com/naito/techpit/chat/chatbachend/api/HelloApiTest.java
+++ src/test/java/com/naito/techpit/chat/chatbachend/api/HelloApiTest.java
@@ -1,15 +1,12 @@
 package com.naito.techpit.chat.chatbachend.api;
 
 import java.net.URL;
-import java.util.stream.Stream;
 import javax.sql.DataSource;
 import org.dbunit.Assertion;
 import org.dbunit.DataSourceDatabaseTester;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.dataset.csv.CsvURLDataSet;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -32,56 +29,40 @@ public class HelloApiTest {
   @Autowired
   private DataSource dataSource;
 
-  @ParameterizedTest
-  @MethodSource("helloTestProvider") // helloTestProviderメソッドによりパラメータを取得
-  public void helloTest(String queryString, String expectedBody, String dbPath) throws Exception {
+  @Test
+  public void helloTest() throws Exception {
 
-    // IDatabaseTesterを使用してDBテストの準備(実際のDBに影響を及ぼさない)
+    // IDatabaseTesterを使用してDB操作の準備
     IDatabaseTester databaseTester = new DataSourceDatabaseTester(dataSource);
-    URL givenUrl = this.getClass().getResource("/hello/hello/" + dbPath + "/given/");
+    URL givenUrl = this.getClass().getResource("/hello/hello/default/given/");
     // CsvURLDataSetでcsv形式のテストデータをセット
     databaseTester.setDataSet(new CsvURLDataSet(givenUrl));
+    // 実行
     databaseTester.onSetup();
 
-
-    // リクエストとレスポンスのテスト
     mockMvc.perform(
         // MockMvcRequestBuilders：テスト用リクエストを作成する
-        MockMvcRequestBuilders.get("/hello" + queryString).accept(MediaType.APPLICATION_JSON))
+        MockMvcRequestBuilders.get("/hello").content("""
+            {
+              "name": "Lavulite"
+            }
+            """).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
         // MockMvcResultMatchers：レスポンスを検証する
         .andExpect(MockMvcResultMatchers.status().isOk())
-        // JSONAssert.assertEquals(expected, actual, strict(比較の厳格さ))
-        .andExpect((result) -> JSONAssert.assertEquals(expectedBody,
-            result.getResponse().getContentAsString(), false));
+        // JSONAssert.assertEquals(expected, actual, strict)
+        .andExpect((result) -> JSONAssert.assertEquals("""
+            {
+              "message": "Hello, world!"
+            }
+              """, result.getResponse().getContentAsString(), false));
 
-    // DBのテスト
-    // var actualDataSet = databaseTester.getConnection().createDataSet();
-    var actualDataSet = databaseTester.getDataSet();
-    var expectedUrl = this.getClass().getResource("/hello/hello/" + dbPath + "/expected/");
+    var actualDataSet = databaseTester.getConnection().createDataSet();
+    var actualTestTable = actualDataSet.getTable("test");
+    var expectedUrl = this.getClass().getResource("/hello/hello/default/expected/");
     var expectedDataSet = new CsvURLDataSet(expectedUrl);
-    // var actualTable = expectedDataSet.getTable("test");
-    // var expectedTable = actualDataSet.getTable("test");
-    Assertion.assertEquals(actualDataSet, expectedDataSet);
+    var expectedTestTable = expectedDataSet.getTable("test");
+    Assertion.assertEquals(expectedTestTable, actualTestTable);
 
-
-  }
-
-  /*
-   * 引数を提供するメソッドの条件
-   * 
-   * - static メソッドである - ArgumentsクラスのStreamを返却する
-   */
-  private static Stream<Arguments> helloTestProvider() {
-    // Arguments.arguments(queryString, expectedBody, dbPath)
-    return Stream.of(Arguments.arguments("", """
-        {
-          "message": "Hello, world!"
-        }
-        """, "default"), Arguments.arguments("?name=techpit", """
-        {
-          "message": "Hello, techpit"
-        }
-        """, "techpit"));
   }
 
 }
