5289fc1cd387bc9471d43c3e93f6718adc756fc3
Naito Yuma <naitoyuma1118@gmail.com>
2023-04-25 01:51:31 +0900

    [ref]@Dataを@RequiredArgsConstructorの変更
    概要:
    @Dateにはコンストラクタ生成機能がないため,Bean登録がされていてもChannelDomainServiceの
    依存性注入が行われずエラーが発生すると考えた。
    @RequiredArgsConstructorに変更し,自動生成されたchannelDomainServiceフィールドに対する
    コンストラクタによりインスタンス化とDIが行われるものと思われる。
    
    コメント:
    実際はなぜか@Dataにおいてもエラーが発生しない
    SpringはIoCコンテナにより管理対象のインスタンス生成/保持/注入を制御してくれる
    newを利用してインスタン化するなど明示的な記述が省略できる反面,自動で何が行われているか理解する必要がある

--- src/main/java/com/naito/techpit/chat/chatbachend/application/service/ChannelService.java
+++ src/main/java/com/naito/techpit/chat/chatbachend/application/service/ChannelService.java
@@ -1,16 +1,15 @@
 package com.naito.techpit.chat.chatbachend.application.service;
 
-import java.util.Collections;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.naito.techpit.chat.chatbachend.domain.channels.model.Channel;
 import com.naito.techpit.chat.chatbachend.domain.channels.service.ChannelDomainService;
-import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
-@Data
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class ChannelService {
   private final ChannelDomainService channelDomainService;
 
