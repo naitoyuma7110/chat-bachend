80acc9d74dc76b59e4d1ebd663467598781edca9
Naito Yuma <naitoyuma1118@gmail.com>
2023-04-25 03:01:15 +0900

    [add]ChannelDomainService内でのChannelRepositoryの利用
    概要:
    ChannelRepositoryの利用しDBから最新のidを取得しModelにセット,
    DBに新しいレコードとして追加する処理を実装
    
    コメント:
    Repository:モデルの永続化の抽象(インターフェース)を利用する事で,
    ドメインはDB処理の具体的な実装方法に配慮する必要がなくなる
    例えば将来DBの仕様が変わってもインターフェースを介する事で主幹処理の内容に変更を加える必要がない

--- src/main/java/com/naito/techpit/chat/chatbachend/domain/channels/service/ChannelDomainService.java
+++ src/main/java/com/naito/techpit/chat/chatbachend/domain/channels/service/ChannelDomainService.java
@@ -4,25 +4,32 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
+import com.naito.techpit.chat.chatbachend.application.service.ChannelRepository;
 import com.naito.techpit.chat.chatbachend.domain.channels.model.Channel;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ChannelDomainService {
+  private final ChannelRepository channelRepository;
 
   public Channel create(Channel channel) {
-    // TODO: フロントエンドからIDを受け取らない仕様
-    // 作成するレコードのIDはDBから取得する必要がある
+    /*
+     * UIからidを受け取らない仕様のためChannelモデルにはnameフィールドのみ値がある
+     * 
+     * 処理内容：DB情報を取得しChannelモデルに最新のidをセットしDB登録する
+     */
 
     // Optional:null許容の型, Optional.of(1)はOptional型に1を含む(null)ではない値
-    Optional<Integer> currentMaxId = Optional.of(1);
+    // var currentMaxId = Optional.of(1);
+    var currentMaxId = channelRepository.getMaxId();
 
     // .orElse:nullの際のデフォルト値,この場合0を設定
     var newid = currentMaxId.orElse(0) + 1;
     channel.setId(newid);
 
-    // TODO: DBに永続化する。
+    // DBに永続化する。
+    channelRepository.insert(channel);
 
     return channel;
   }
